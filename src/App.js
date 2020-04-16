883bfd97e02e5f8887c032cbea39c84e2e0bfb65
juicecultus <juicecultus@gmail.com>
2020-04-16 17:34:59 +0100

    Hooks - useState

--- src/App.js
+++ src/App.js
@@ -3,24 +3,6 @@ import { render } from 'react-dom';
 import SearchParams from './SearchParams';
 
 const App = () => {
-  // return React.createElement('div', {}, [
-  //   React.createElement('h1', { id: 'something-important' }, 'Adopt Me!'),
-  //   React.createElement(Pet, {
-  //     name: 'Luna',
-  //     animal: 'Dog',
-  //     breed: 'Havanese',
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: 'Pepper',
-  //     animal: 'Bird',
-  //     breed: 'Cockatiel',
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: 'Doink',
-  //     animal: 'Cat',
-  //     breed: 'Mixed',
-  //   }),
-  // ]);
   return (
     <div>
       <h1 id='something important'>Adopt Me!</h1>
